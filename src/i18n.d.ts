3d7d65a6d5d13b364bff32dc8821957f145285fe
magin <maginciancompany@gmail.com>
2025-10-11 18:09:04 +0200

    Autocompleted

---
+++ src/i18n.d.ts
@@ -0,0 +1,15 @@
+import type translation from '../public/locales/en/translation.json' 
+
+// Amplía el módulo i18next
+declare module 'i18next' {
+  interface CustomTypeOptions {
+    // 1. Define el namespace por defecto (si lo usas)
+    defaultNS: 'translation'; 
+    
+    // 2. Define la estructura de tus recursos
+    resources: {
+      translation: typeof translation;
+      // Si tienes más namespaces, añádelos aquí (ej: 'common': typeof commonJson)
+    };
+  }
+}
