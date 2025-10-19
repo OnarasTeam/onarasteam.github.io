a0c4052f9b2beefb89bf03ba982f6d7e89d3ba79
magin <maginciancompany@gmail.com>
2025-10-19 12:16:29 +0200

    try about in deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import Main from './pages/Main'
 import NotFound from './pages/NotFound'
+import About from './pages/About'
 
 function App() {
   return (
     <div id='main-container'>
       <Routes>
         <Route path='/' element={<Main />} />
+        <Route path='/about' element={<About />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
