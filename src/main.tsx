45085e39af207b779780693bd3561442c5c83cb3
magin <maginciancompany@gmail.com>
2025-10-19 12:22:13 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './i18n'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 
 console.log(import.meta.env.BASE_URL)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL || '/'}>
+    <HashRouter basename={import.meta.env.BASE_URL || '/'}>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
